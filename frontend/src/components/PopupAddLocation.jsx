728fca8f1d2c1dba97e21cff69e8556ff05cdc13
thiagosullivan <thiagosullivan12@gmail.com>
2025-09-29 15:12:12 -0300

    (frontend) feat: set new toasts on add location form

--- frontend/src/components/PopupAddLocation.jsx
+++ frontend/src/components/PopupAddLocation.jsx
@@ -1,12 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner"
+import { LocationContext } from "../contexts/LocationContext";
+import { Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
 import { Input } from "./ui/input";
-import { LocationContext } from "../contexts/LocationContext";
-import { Loader2 } from "lucide-react";
-import { toast } from "sonner"
 
 export function PopupAddLocation({ open, setOpen, newLocationData }) {
   const { addLocation } = useContext(LocationContext);
@@ -20,6 +20,7 @@ export function PopupAddLocation({ open, setOpen, newLocationData }) {
   });
 
   async function onSubmit(values) {
+    setLoading(true)
     try {
         const updatedLocationData = {
           ...newLocationData,
@@ -36,9 +37,10 @@ export function PopupAddLocation({ open, setOpen, newLocationData }) {
         form.reset();
         
     } catch (error) {
-        console.log(error, 'Erro ao enviar o formulário')
-        toast.error('Nova localização adicionada com sucesso!')
+        console.log(error, 'Erro ao enviar o formulário.')
+        toast.error('Ocorreu um erro ao enviar o formulário.')
     } finally {
+        setLoading(false)
         toast.success('Nova localização adicionada com sucesso!')
     }
 
@@ -55,59 +57,59 @@ export function PopupAddLocation({ open, setOpen, newLocationData }) {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-          <DialogContent className="sm:max-w-[425px]">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-            <DialogHeader className="mb-4">
-              <DialogTitle>Nova localização no mapa</DialogTitle>
-              {newLocationData ? (
-                <DialogDescription>
-                  Local em {newLocationData.geometry.coordinates[0].toFixed(4)}, {newLocationData.geometry.coordinates[1].toFixed(4)}
-                </DialogDescription>
-              ) : (
-                <DialogDescription>Carregando localização...</DialogDescription>
-              )}
-            </DialogHeader>
-            <div className="grid gap-4 mb-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Título</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+              <DialogHeader className="mb-4">
+                <DialogTitle>Nova localização no mapa</DialogTitle>
+                {newLocationData ? (
+                  <DialogDescription>
+                    Local em {newLocationData.geometry.coordinates[0].toFixed(4)}, {newLocationData.geometry.coordinates[1].toFixed(4)}
+                  </DialogDescription>
+                ) : (
+                  <DialogDescription>Carregando localização...</DialogDescription>
                 )}
-              />
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Descrição</FormLabel>
-                    <FormControl>
-                      <Input disabled={loading} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button type="button" variant="outline">Cancelar</Button>
-              </DialogClose>
-              <Button type="submit" disabled={loading}>
-                {loading && <Loader2 className="animate-spin h-4 w-4" />}
-                Salvar
-              </Button>
-            </DialogFooter>
-        </form>
-      </Form>
-          </DialogContent>
+              </DialogHeader>
+              <div className="grid gap-4 mb-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Título</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Descrição</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="button" variant="outline">Cancelar</Button>
+                </DialogClose>
+                <Button type="submit" disabled={loading}>
+                  {loading && <Loader2 className="animate-spin h-4 w-4" />}
+                  Salvar
+                </Button>
+              </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
   );
 }
