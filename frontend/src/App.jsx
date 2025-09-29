a6a6da78b3cd39f3428240deb669e048086fe5c0
thiagosullivan <thiagosullivan12@gmail.com>
2025-09-29 09:16:20 -0300

    (frontend) feat: added and configured Shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
-    <div className="bg-orange-600">
-      <h1 className="text-green-500">Hello World</h1>
+    <div className="">
+      <h1 className="">Hello World</h1>
+      <Button className="bg-primary">Clickar</Button>
     </div>
   )
 }
