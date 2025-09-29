a948a2c534bd21e6bea6152d8383b2ac78fc7bef
thiagosullivan <thiagosullivan12@gmail.com>
2025-09-29 09:30:51 -0300

    (frontend) feat: added Sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import { Button } from "./components/ui/button"
+import { AppSidebar } from "./components/AppSidebar";
+import { Button } from "./components/ui/button";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { ArrowBigLeft } from "lucide-react";
 
 function App() {
 
   return (
-    <div className="">
-      <h1 className="">Hello World</h1>
-      <Button className="bg-primary">Clickar</Button>
-    </div>
+    <SidebarProvider>
+        <div className="flex w-full">
+          <AppSidebar />
+          <main className="w-full">
+            <div className="h-screen"
+                  style={{
+                    backgroundImage: `
+                      linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+                      url(/brasilia-bg.jpg)
+                    `,
+                  }}
+            >
+              <SidebarTrigger className="mb-2" />
+              <div className="flex mb-4 justify-center items-center">
+                <ArrowBigLeft className="h-10 w-10" />
+                <h1 className="text-center text-xl font-bold ml-2"> Você pode verificar, excluir ou salvar os endereços no menu à esquerda</h1>
+              </div>
+              {/* <Locations /> */}
+            </div>
+              {/* <Footer /> */}
+          </main>
+        </div>
+      </SidebarProvider>
   )
 }
 
---
+++ frontend/src/components/AppSidebar.jsx
@@ -0,0 +1,78 @@
+import { Loader2, MapPin, Pencil, Trash2 } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+} from "../components/ui/sidebar"
+// import { useLocation } from "../hooks/use-location";
+import { Button } from "./ui/button";
+// import { PopupUpdateLocation } from "./PopupUpdateLocation";
+import { useState } from "react";
+
+
+export function AppSidebar() {
+//   const { locations, updateLocations, deleteSavedLocation } = useLocation();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+//   const [locationDataToUpdate, setlocationDataToUpdate] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+//   async function delLocationHandler(location){
+//     setLoading(true)
+//     try {
+//       await deleteSavedLocation(location.id);
+//     } catch (error) {
+//       console.log(error)
+//     }
+//     setLoading(false)
+//   }
+  
+//   async function updateLocationHandler(location){
+//     setIsDialogOpen(true)
+//     setlocationDataToUpdate(location)
+//   }
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="p-5 mb-4">
+            <img src="./e-topocart.png" />
+          </SidebarGroupLabel>
+          <div className="border-b border-gray-400" />
+          <SidebarGroupContent className="px-2 py-6">
+            <div className="flex items-center gap-2">
+              <MapPin />
+              <p className="text-2xl">Marcações</p>
+            </div>
+            <SidebarMenu>
+              {/* {locations.map((location) => (
+                <SidebarMenuItem key={location.id}>
+                    <div className="flex justify-between border-t  pt-3 mt-5">
+                      <p className="text-xl truncate">{location.properties.name}</p>
+                      <div className="flex justify-end gap-1">
+                        <Button variant="outline" onClick={() => updateLocationHandler(location)} className="h-8 w-8">
+                          <Pencil />
+                        </Button>
+                        <Button onClick={() => delLocationHandler(location)} className="h-8 w-8" disabled={loading}>
+                          {loading ? <Loader2 className="animate-spin h-4 w-4"/> : <Trash2 />}
+                        </Button>
+                      </div>
+                    </div>
+                </SidebarMenuItem>
+              ))} */}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {/* <PopupUpdateLocation
+        open={isDialogOpen}
+        setOpen={setIsDialogOpen}
+        locationDataToUpdate={locationDataToUpdate}
+      /> */}
+    </Sidebar>
+  )
+}
